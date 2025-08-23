935527a81ceac56af5d722856bb47069203bd95b
NicolasDoneda <nicolasdoneda231@gmail.com>
2025-08-22 22:41:02 -0300

    a

---
+++ Aula22-08/resources/views/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de API</title>
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+    <div class = "bg-red-800">
+        <h1 class = "text-3xl text-red-200 p-9 text-center"> Teste de API</h1>
+    </div>
+    <div class = "bg-blue-500 m-20" >
+        <p class = "text-5xl text-center p-50 text-blue-200"> Banner</p>
+    </div>
+    <div class ="flex justify-center align-center m-20 flex-wrap gap-10 pb-70">
+        
+        <div class = "bg-purple-100 w-213 h-60"><p class = "text-purple-800 text-center">100</p></div>
+        <div class = "bg-purple-200 w-213 h-60"><p class = "text-purple-700 text-center">200</p></div>
+        <div class = "bg-purple-300 w-213 h-60"><p class = "text-purple-600 text-center">300</p></div>
+        <div class = "bg-purple-400 w-213 h-60"><p class = "text-purple-500 text-center">400</p></div>
+        <div class = "bg-purple-500 w-213 h-60"><p class = "text-purple-400 text-center">500</p></div>
+        <div class = "bg-purple-600 w-213 h-60"><p class = "text-purple-300 text-center">600</p></div>
+        <div class = "bg-purple-700 w-213 h-60"><p class = "text-purple-200 text-center">700</p></div>
+        <div class = "bg-purple-800 w-213 h-60"><p class = "text-purple-100 text-center">800</p></div>
+    
+    </div>
+    <div class ="bg-gray-800 p-15"> </div>
+</body>
+</html>
